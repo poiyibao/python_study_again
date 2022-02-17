70008c9adf7c338724455721ff9cb97f04ba5437
poiyibao <1612409282@qq.com>
2022-02-17 19:04:41 +0800

    sencond

---
+++ again/demo_1.py
@@ -0,0 +1,5 @@
+# sort() & sorted()
+list_1 = [ "bmw" ,"audi" ,"toyota", "subaru"]
+list_2 = list_1.sorted()
+#list_3 = list_1.sorted()
+print(list_2)
--- again/demo_2.py
+++ again/demo_2.py
@@ -1,4 +1,20 @@
+# f字符串
 first_name = "poi"
 last_name = "yibao"
 name = f'{first_name.title()}{last_name.title()}'
 print(name)
+# rstrip()
+string_1 = " empty "
+string_1 = string_1.rstrip()
+print(string_1)
+
+#insert(num,index)在任意位置添加新元素；append(index)在列表尾添加
+
+dic ={
+  'color': 'blue',
+  'num': 13,
+  'max': 'maxkey',
+  'min': 'minkey'
+	}
+for k, v in dic.items():
+    print(f"{k}={v}")
